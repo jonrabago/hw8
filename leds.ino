e8cea0f28e8e14fb9d57f436519ca10ef1e6cd90
jonrabago <55300118+jonrabago@users.noreply.github.com>
2019-11-02 23:12:36 -0700

    Create leds.ino

---
+++ leds.ino
@@ -0,0 +1,16 @@
+int led = 13;
+int led2 = 12;
+
+void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop () {
+  digitalWrite(led, HIGH);
+  digitalWrite(led2, LOW);
+  delay(2000);
+  digitalWrite(led, LOW);
+  digitalWrite(led2, HIGH);
+  delay(1000);
+}
